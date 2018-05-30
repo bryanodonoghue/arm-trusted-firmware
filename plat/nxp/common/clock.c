36bb57a795eb8fa3acc3d6cbdfcd575c687e94e3
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-05-30 12:13:51 +0100

    imx: clock: Provide a clock gate enable/disable routine
    This patch provides a clock-gate enable/disable routine via the set/clr
    registers in a given clock-gate control register block.
    
    This index passed should be one of the enums associated with CCM and
    depending on enable/disable being passed either set or clr will be written
    to.
    
    The Domain0 bits are currently the only bits targeted by this write, more
    work may need to be done on the domain bits in subsequent patches as a
    result.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- plat/nxp/common/clock.c
+++ plat/nxp/common/clock.c
@@ -3,9 +3,29 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-
+#include <arch.h>
+#include <mmio.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include <imx-regs.h>
 #include "clock.h"
+
+static void mxc_clock_gate_enable(unsigned int id, bool enable)
+{
+	struct ccm *ccm = ((struct ccm *)CCM_BASE);
+	uintptr_t addr;
+
+	if (id > CCM_CLK_GATE_CTRL_NUM)
+		return;
+
+	/* TODO: add support for more than DOMAIN0 clocks */
+	if (enable)
+		addr = (uintptr_t)&ccm->ccm_clk_gate_ctrl[id].ccm_ccgr_set;
+	else
+		addr = (uintptr_t)&ccm->ccm_clk_gate_ctrl[id].ccm_ccgr_clr;
+
+	mmio_write_32(addr, CCM_CCGR_SETTING0_DOM_CLK_ALWAYS);
+}
 
 void clock_init(void)
 {
