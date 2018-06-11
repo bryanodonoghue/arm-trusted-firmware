c73024b64051303fa8e0f9400b2bfd66930d2990
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-06-11 15:42:19 +0100

    warp7: io_storage: Define open_memmap() for FIP in memory
    Using u-boot to bootstrap ATF in early stage development means we want to
    memmap() a FIP image already loaded into DRAM by u-boot.
    
    The open_memmap() routine is pretty standard across platforms that
    implement it and is itself deserving of a patch to rationalize the
    implementation of this function throughout the various ATF ports.
    
    This patch replicates open_memmap() with a view to superseding the
    open_memmap() implementation with something to read the FIP out of eMMC
    once we are producing eMMC images with the FIP embedded inside.
    
    For now we'll let u-boot do the heavy lifting and map the FIP it has placed
    in RAM for us.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- plat/nxp/imx7/warp7/warp7_io_storage.c
+++ plat/nxp/imx7/warp7/warp7_io_storage.c
@@ -3,13 +3,19 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
+#include <debug.h>
 #include <io_block.h>
+#include <io_memmap.h>
 #include <platform_def.h>
+
+static uintptr_t memmap_dev_handle;
 
 static const io_block_spec_t fip_block_spec = {
 	.offset = WARP7_FIP_BASE,
 	.length = WARP7_FIP_SIZE
 };
+
+static int open_memmap(const uintptr_t spec);
 
 /* TODO: this structure is replicated multiple times. rationalize it ! */
 struct plat_io_policy {
@@ -17,6 +23,22 @@ struct plat_io_policy {
 	uintptr_t image_spec;
 	int (*check)(const uintptr_t spec);
 };
+
+static int open_memmap(const uintptr_t spec)
+{
+	int result;
+	uintptr_t local_image_handle;
+
+	result = io_dev_init(memmap_dev_handle, (uintptr_t)NULL);
+	if (result == 0) {
+		result = io_open(memmap_dev_handle, spec, &local_image_handle);
+		if (result == 0) {
+			VERBOSE("Using Memmap\n");
+			io_close(local_image_handle);
+		}
+	}
+	return result;
+}
 
 int plat_get_image_source(unsigned int image_id, uintptr_t *dev_handle,
 			  uintptr_t *image_spec)
