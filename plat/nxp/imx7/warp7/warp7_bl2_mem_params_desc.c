e11c61990eb1d9dcf33f3b02af602e32e3219425
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-06-11 13:39:20 +0100

    warp7: mem_params_desc: Add BL32 entry to mem params array
    Describes the location and size of the OPTEE image to be loaded.
    Requires a FIP image to be loaded at a known address.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- plat/nxp/imx7/warp7/warp7_bl2_mem_params_desc.c
+++ plat/nxp/imx7/warp7/warp7_bl2_mem_params_desc.c
@@ -10,6 +10,21 @@
 #include <platform_def.h>
 
 static bl_mem_params_node_t bl2_mem_params_descs[] = {
+	{
+		.image_id = BL32_IMAGE_ID,
+
+		SET_STATIC_PARAM_HEAD(ep_info, PARAM_EP, VERSION_2,
+				      entry_point_info_t, SECURE | EXECUTABLE),
+		.ep_info.pc = BL32_BASE,
+
+		SET_STATIC_PARAM_HEAD(image_info, PARAM_EP, VERSION_2,
+				      image_info_t, 0),
+
+		.image_info.image_base = WARP7_OPTEE_BASE,
+		.image_info.image_max_size = WARP7_OPTEE_SIZE,
+
+		.next_handoff_image_id = BL33_IMAGE_ID,
+	},
 };
 
 REGISTER_BL_IMAGE_DESCS(bl2_mem_params_descs);
