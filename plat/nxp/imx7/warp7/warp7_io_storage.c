14402d0e0554b42fd36683932241d0c9e85a4f8e
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-06-11 14:40:16 +0100

    warp7: io_storage: Add the struct io_policy structure
    A standard means of describing how to load images is via the struct
    io_policy structure. Oddly this structure has been copy/pasted a number of
    times in various platform ports.
    
    A big TOOD: here is to rationalize that down to one declaration and
    propagate to the various users of it.
    
    This patch replicates the definition and should go away in a rebase which
    takes care of defining struct io_policy {}; correctly in some future patch.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- plat/nxp/imx7/warp7/warp7_io_storage.c
+++ plat/nxp/imx7/warp7/warp7_io_storage.c
@@ -3,8 +3,14 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-
 #include <platform_def.h>
+
+/* TODO: this structure is replicated multiple times. rationalize it ! */
+struct plat_io_policy {
+	uintptr_t *dev_handle;
+	uintptr_t image_spec;
+	int (*check)(const uintptr_t spec);
+};
 
 int plat_get_image_source(unsigned int image_id, uintptr_t *dev_handle,
 			  uintptr_t *image_spec)
