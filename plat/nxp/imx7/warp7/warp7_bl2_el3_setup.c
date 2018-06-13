ae67a88354a9e3686aec39f89309e3f633175a93
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-06-13 11:24:44 +0100

    warp7: u-boot: Provide a plat_get_ns_image_entrypoint
    As the name describes, this implements a simple return to derive the
    entry-point of the non-secure image.
    
    This get() method is the preferred way of accessing this data in ATF.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- plat/nxp/imx7/warp7/warp7_bl2_el3_setup.c
+++ plat/nxp/imx7/warp7/warp7_bl2_el3_setup.c
@@ -10,6 +10,7 @@
 #include <console.h>
 #include <debug.h>
 #include <desc_image_load.h>
+#include <optee_utils.h>
 #include <platform_def.h>
 #include <xlat_mmu_helpers.h>
 #include <xlat_tables_defs.h>
@@ -21,15 +22,66 @@
 #define UART1_CLK_SELECT (CCM_TARGET_ROOT_ENABLE |\
 			  CCM_TRGT_MUX_UART1_CLK_ROOT_OSC_24M)
 
+uintptr_t plat_get_ns_image_entrypoint(void)
+{
+	return WARP7_UBOOT_BASE;
+}
+
 static uint32_t warp7_get_spsr_for_bl32_entry(void)
 {
 	return SPSR_MODE32(MODE32_svc, SPSR_T_ARM, SPSR_E_LITTLE,
 			   DISABLE_ALL_EXCEPTIONS);
 }
 
+#ifndef AARCH32_SP_OPTEE
+#error "Must build with OPTEE support included"
+#endif
+
 int bl2_plat_handle_post_image_load(unsigned int image_id)
 {
-	return 0;
+	int err = 0;
+	bl_mem_params_node_t *bl_mem_params = get_bl_mem_params_node(image_id);
+
+	bl_mem_params_node_t *pager_mem_params = NULL;
+	bl_mem_params_node_t *paged_mem_params = NULL;
+
+	assert(bl_mem_params);
+
+	switch (image_id) {
+	case BL32_IMAGE_ID:
+		pager_mem_params = get_bl_mem_params_node(BL32_EXTRA1_IMAGE_ID);
+		assert(pager_mem_params);
+
+		paged_mem_params = get_bl_mem_params_node(BL32_EXTRA2_IMAGE_ID);
+		assert(paged_mem_params);
+
+		err = parse_optee_header(&bl_mem_params->ep_info,
+					 &pager_mem_params->image_info,
+					 &paged_mem_params->image_info);
+		if (err != 0)
+			WARN("OPTEE header parse error.\n");
+
+		/*
+		 * When ATF loads the DTB the address of the DTB is passed in
+		 * arg2, however for the WaRP7 we will have u-boot load the DTB
+		 * directly and therefore we won't be modifying the DTB in
+		 * OPTEE.
+		 * Just leave arg2 @ 0 for now - unless we deicide to switch up
+		 * the responsibility for loading DTB from u-boot to ATF
+		 */
+		bl_mem_params->ep_info.args.arg0 =
+					bl_mem_params->ep_info.args.arg1;
+		bl_mem_params->ep_info.args.arg1 = 0;
+		bl_mem_params->ep_info.args.arg2 = 0;
+		bl_mem_params->ep_info.args.arg3 = 0;
+		bl_mem_params->ep_info.spsr = warp7_get_spsr_for_bl32_entry();
+		break;
+	default:
+		/* Do nothing in default case */
+		break;
+	}
+
+	return err;
 }
 
 void bl2_el3_plat_arch_setup(void)
