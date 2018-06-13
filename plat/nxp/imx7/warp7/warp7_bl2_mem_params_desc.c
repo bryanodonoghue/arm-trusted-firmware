e9e9a86b8f75fcdb82cf298bc8a63efc0e73d865
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-06-13 09:50:24 +0100

    warp7: mem_params_desc: Add BL32_EXTRA1 entry to mem params array
    In order to both correctly load images out of the FIP and to pass
    parameters to those images from ATF, we need a memory parameters entry in
    bl2_mem_params_descs.
    
    This adds BL32_EXTRA1_IMAGE_ID to that array.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- plat/nxp/imx7/warp7/warp7_bl2_mem_params_desc.c
+++ plat/nxp/imx7/warp7/warp7_bl2_mem_params_desc.c
@@ -25,6 +25,20 @@ static bl_mem_params_node_t bl2_mem_params_descs[] = {
 
 		.next_handoff_image_id = BL33_IMAGE_ID,
 	},
+	{
+		.image_id = BL32_EXTRA1_IMAGE_ID,
+
+		SET_STATIC_PARAM_HEAD(ep_info, PARAM_EP, VERSION_2,
+				      entry_point_info_t,
+				      SECURE | NON_EXECUTABLE),
+
+		SET_STATIC_PARAM_HEAD(image_info, PARAM_EP, VERSION_2,
+				      image_info_t, IMAGE_ATTRIB_SKIP_LOADING),
+		.image_info.image_base = WARP7_OPTEE_BASE,
+		.image_info.image_max_size = WARP7_OPTEE_SIZE,
+
+		.next_handoff_image_id = INVALID_IMAGE_ID,
+	},
 };
 
 REGISTER_BL_IMAGE_DESCS(bl2_mem_params_descs);
