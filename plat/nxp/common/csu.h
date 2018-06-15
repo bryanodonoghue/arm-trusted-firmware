f95d0e006c75b90dc9b70993b90b8c8f2e351ca0
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-06-15 13:36:24 +0100

    imx: csu: Add csu.h
    Add a header to define csu_init(). Later patches will add in
    bit-definitions for the CSU registers.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

---
+++ plat/nxp/common/csu.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef __CSU_H__
+#define __CSU_H__
+
+#include <arch.h>
+
+void csu_init(void);
+
+#endif /* __CSU_H__ */
