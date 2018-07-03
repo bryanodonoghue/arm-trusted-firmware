7d69e089815c197b9ee706ad4e9c2a6b4ae9e0e1
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-07-03 15:11:33 +0100

    imx7: hab: Provide a hab_arch.h file
    In order to enable compile time differences in HAB interaction, we should
    split out the definition of the base address of the HAB API.
    
    Some version of the i.MX series have different offsets from the BootROM
    base for the HAB callback table.
    
    This patch defines the header into which we will define the i.MX7 specific
    offset. Later ATF implementations for different NXP processors with HAB
    should then define the appropriate offset for the used CPU.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Reviewed-by: Ryan Harkin <ryan.harkin@linaro.org>

---
+++ plat/nxp/imx7/include/hab_arch.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2018, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef __HAB_ARCH_H__
+#define __HAB_ARCH_H__
+
+#endif /* __HAB_ARCH_H__ */
