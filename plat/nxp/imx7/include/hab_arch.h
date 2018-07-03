05d496703e69c79080e240c3b165ba2a4443ed0a
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-07-03 15:14:32 +0100

    imx7: hab_arch: Define the offset of the HAB API on i.MX7
    This patch defines the offset from the BootROM base address of the HAB
    function pointer table.
    
    We can latch a set of common function pointer locations from the offset
    given here and if necessary change the offset for different processors
    without any other code-change.
    
    For now all we support is i.MX7 so the only offset being defined is that
    for the i.MX7 here.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Reviewed-by: Ryan Harkin <ryan.harkin@linaro.org>

--- plat/nxp/imx7/include/hab_arch.h
+++ plat/nxp/imx7/include/hab_arch.h
@@ -6,4 +6,7 @@
 #ifndef __HAB_ARCH_H__
 #define __HAB_ARCH_H__
 
+/* Define the offset the High Assurance Boot callback table is at */
+#define HAB_CALLBACK_OFFSET	0x100
+
 #endif /* __HAB_ARCH_H__ */
