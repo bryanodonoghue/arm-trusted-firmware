5667e588cee995cd9d70395778af82429db971d2
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-07-03 15:07:32 +0100

    imx: hab: Define a HAB header file
    The High Assurance Boot or HAB is an on-chip method of providing a
    root-of-trust from the reset vector to subsequent stages in the bootup
    flow of the Cortex-A7 on the i.MX series of processors.
    
    This patch adds a simple header file to be fleshed out with a minimal set
    of HAB bindings useful to ATF.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Reviewed-by: Ryan Harkin <ryan.harkin@linaro.org>

---
+++ plat/nxp/common/hab.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2018, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef __HAB_H__
+#define __HAB_H__
+
+#include <imx-regs.h>
+
+#endif /* __HAB_H__ */
