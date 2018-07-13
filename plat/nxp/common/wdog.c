f51001ab4597b436b8645297f844f9612692f64f
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-07-13 10:27:49 +0100

    imx: wdog: Add code to set watchdog power-down
    The i.MX7 Solo Applications Processor Reference Manual details the PDE bit
    as follows:
    
    "Power Down Enable bit. Reset value of this bit is 1, which means the power
    down counter inside the WDOG is enabled after reset. The software must
    write 0 to this bit to disable the counter within 16 seconds of reset
    de-assertion. Once disabled this counter cannot be enabled again. See
    Power-down counter event for operation of this counter."
    
    This patch does that zero write in-lieu of later phases in the boot
    no-longer have the necessary permissions to rewrite the PDE bit directly.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- plat/nxp/common/wdog.c
+++ plat/nxp/common/wdog.c
@@ -5,7 +5,22 @@
  */
 
 #include <imx-regs.h>
+#include <wdog.h>
+#include <mmio.h>
+
+#include <debug.h>
+
+static void wdog_power_down(unsigned long base)
+{
+	struct wdog_regs *wdog = (struct wdog_regs *)base;
+
+	mmio_write_16((uintptr_t)&wdog->wmcr, 0);
+}
 
 void wdog_init(void)
 {
+	wdog_power_down(WDOG1_BASE);
+	wdog_power_down(WDOG2_BASE);
+	wdog_power_down(WDOG3_BASE);
+	wdog_power_down(WDOG4_BASE);
 }
